c341109da4865eb542b4708c769218935e44a376
daichi2mori <daichi2mori@gmail.com>
2024-07-08 11:52:52 +0900

    fix: typo

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,14 @@
 import { Database } from "bun:sqlite";
 
-const dbFileName = "todos";
-const tableName = "database.sqlite";
+const dbFileName = "database.sqlite";
+const tableName = "todos";
 
 // ファイルが存在しない場合、データベースを作成する
-const db = new Database(tableName, { create: true });
+const db = new Database(dbFileName, { create: true });
 
 const query = db.query(
   "CREATE TABLE IF NOT EXISTS " +
-    dbFileName +
+    tableName +
     " (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, completed BOOLEAN NOT NULL DEFAULT false"
 );
 
