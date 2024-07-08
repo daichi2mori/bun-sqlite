9e6c4324fc658f2f207d5adc268f9b8a7868b5ce
daichi2mori <daichi2mori@gmail.com>
2024-07-08 10:59:22 +0900

    refactor: function separation

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,17 @@
 import { Database } from "bun:sqlite";
 
-const db = new Database("database.sqlite");
+const dbFileName = "todos";
+const tableName = "database.sqlite";
 
-db.run(`
-  CREATE TABLE IF NOT EXISTS todos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    completed BOOLEAN NOT NULL DEFAULT false
-  )
-`);
+// ファイルが存在しない場合、データベースを作成する
+const db = new Database(tableName, { create: true });
+
+const query = db.query(
+  "CREATE TABLE IF NOT EXISTS " +
+    dbFileName +
+    " (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, completed BOOLEAN NOT NULL DEFAULT false"
+);
+
+query.run();
 
 export default db;
