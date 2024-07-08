c6e2affb4c67e18f6c2617050fdcf4fa16054cc9
daichi2mori <daichi2mori@gmail.com>
2024-07-08 13:07:18 +0900

    fix: query

--- src/db.ts
+++ src/db.ts
@@ -6,10 +6,12 @@ const tableName = "todos";
 // ファイルが存在しない場合、データベースを作成する
 const db = new Database(dbFileName, { create: true });
 
-const query = db.query(
-  `CREATE TABLE IF NOT EXISTS ${tableName} (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, completed BOOLEAN NOT NULL DEFAULT false`
-);
-
-query.run();
+db.run(`
+  CREATE TABLE IF NOT EXISTS ${tableName} (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT false
+  )
+`);
 
 export default db;
