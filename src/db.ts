17700eb7f1d48a53419ec180b3663664141800a0
daichi2mori <daichi2mori@gmail.com>
2024-07-10 16:44:23 +0900

    add: comment

--- src/db.ts
+++ src/db.ts
@@ -6,6 +6,7 @@ const tableName = "todos";
 // ファイルが存在しない場合、データベースを作成する
 const db = new Database(dbFileName, { create: true });
 
+// テーブルが存在しない場合作成する
 db.run(`
   CREATE TABLE IF NOT EXISTS ${tableName} (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
